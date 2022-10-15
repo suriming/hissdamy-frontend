6f914ffd3c79ab87e037cfb0560d420d50eed4ef
suriming <soolim520@gmail.com>
2022-10-15 18:10:10 +0900

    feat/router

--- src/App.js
+++ src/App.js
@@ -2,17 +2,48 @@ import { Fragment } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
+import SetPose from "./pages/SetPose";
+import styled from "styled-components";
+import top from "./assets/top.png";
 
 function App() {
   return (
     <Fragment>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
+        <Container>
+          <img src={top} />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="pose" element={<SetPose />} />
+          </Routes>
+        </Container>
       </BrowserRouter>
     </Fragment>
   );
 }
+
+const Container = styled.div`
+  min-height: 100vh;
+  width: 100vw;
+  min-width: 35rem;
+  margin: 0 auto;
+  overflow: hidden;
+  // 스크롤바 없앰
+  -ms-overflow-style: none; /* IE and Edge */
+  scrollbar-width: none; /* Firefox */
+
+  &::-webkit-scrollbar {
+    display: none; /* Chrome, Safari, Opera*/
+  }
+
+  @media screen and (min-width: 600px) {
+    width: 35.2rem;
+  }
+`;
+
+// const MainWrapper = styled.div`
+//     // Navbar 높이 확보
+//     padding-top: ${(props) => (props.navbar ? '4.4rem' : '')};
+// `;
 
 export default App;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,23 @@
 import styles from "./Home.module.css";
 import top from "../assets/top.png";
 import DetectPose from "../components/DetectPose";
+import { useNavigate } from "react-router";
 
 function Home() {
   const { container, mainWrapper } = styles;
+  const navigate = useNavigate();
+  const onClick = () => {
+    navigate("/pose");
+  };
   return (
-    <div className={container}>
-      <div className={mainWrapper}>
-        <img src={top} />
-        <DetectPose />
-      </div>
-    </div>
+    <>
+      {/* <div className={container}>
+        <div className={mainWrapper}> */}
+      {/* <img src={top} /> */}
+      {/* </div>
+      </div> */}
+      <button onClick={onClick}>측정 시작</button>
+    </>
   );
 }
 
